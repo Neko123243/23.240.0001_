3920833a33f1153db2e0b31d8f61a798d60b3ad2
Neko123243 <the041663@gmail.com>
2024-10-12 16:29:56 +0700

    Delete Pemrograman_Web_II (MODULE) directory

--- Pemrograman_Web_II (MODULE)/namespace.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once "conflict.php";
-    $conflict1 = new data\one\conflict();
-    $conflict2 = new data\two\conflict();
-?>
--- Pemrograman_Web_II (MODULE)/objeklocation.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once "location.php";
-
-    //membuat objek
-    $obj1 = new location();
-    $obj2 = new city();
-?>
